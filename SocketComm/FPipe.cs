e140c3b2ea8f3ce788d62edece917085ba80c57e
Adrian Plesner <adrian.plesner@gmail.com>
2023-02-28 11:15:42 +0100

    do logging

--- SocketComm/FPipe.cs
+++ SocketComm/FPipe.cs
@@ -272,13 +272,13 @@ public class FPipe : IDisposable, IAsyncDisposable, IEnumerable<Cmd>, IAsyncEnum
         byte[] buffer = new byte[4];
         S.Receive(buffer);
         var length = SimpleConversion.BytesToNumber<int>(buffer);
-        Console.WriteLine($"Receiving {length} bytes!");
+        Logger.LogInformation($"Receiving {length} bytes!");
         buffer = new byte[length];
         var rec = S.Receive(buffer);
         if (rec != length)
         {
             //TODO: throw error, maybe try repeat
-            Console.WriteLine($"Expected {length}, received {rec}");
+            Logger.LogError($"Expected {length}, received {rec}");
         }
 
         WriteCmd(Ok);
